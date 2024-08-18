c62c369cd7408be871ff50f3119e1c45d1b2da32
Pahrijal Saban Mubarok <kevinsudrajat27@gmail.com>
2024-08-18 17:31:34 +0700

    remove unused code

--- application/views/home/addProjectForm.php
+++ application/views/home/addProjectForm.php
@@ -46,17 +46,3 @@
     </div>
   </div>
 </div>
-<script type="text/javascript">
-   $(function () {
-       $('#datetimepicker6').datetimepicker();
-       $('#datetimepicker7').datetimepicker({
-   useCurrent: false //Important! See issue #1075
-   });
-       $("#datetimepicker6").on("dp.change", function (e) {
-           $('#datetimepicker7').data("DateTimePicker").minDate(e.date);
-       });
-       $("#datetimepicker7").on("dp.change", function (e) {
-           $('#datetimepicker6').data("DateTimePicker").maxDate(e.date);
-       });
-   });
-</script>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -3,22 +3,17 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Manajemen Proyek</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.15.1/moment.min.js"></script>
-<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script> -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/js/bootstrap-datetimepicker.min.js"></script>
-
-<!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css"> -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.7.14/css/bootstrap-datetimepicker.min.css">
 
   </head>
   <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
       <div class="container">
         <div class="container-fluid">
-          <a class="navbar-brand" href="#">Manajemen Proyek</a>
+          <a class="navbar-brand" href="<?=base_url(); ?>">Manajemen Proyek</a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
